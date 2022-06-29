7d56d7dd37d06c19295ed85f3b43343470e1a209
Nandu Dharmapalan <nandud999@gmail.com>
2022-06-29 11:51:24 -0400

    Sorting repos reverse chronologically by its creation date

--- web/src/components/Repositories/TableRow/TableRow.tsx
+++ web/src/components/Repositories/TableRow/TableRow.tsx
@@ -7,6 +7,11 @@ interface TableRowProps {
 export function TableRow(props: TableRowProps) {
   const { repos } = props;
 
+  //Sorting repos reverse chronologically by its creation date
+  repos.sort((x, y) => {
+    return new Date(y.created_at).getTime() - new Date(x.created_at).getTime();
+  });
+
   return (
     <>
       {repos.map((repo, index) => (
