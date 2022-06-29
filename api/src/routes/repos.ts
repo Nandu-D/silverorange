d6b2214d4419c186e15f23dca202e0937a637bee
Nandu Dharmapalan <nandud999@gmail.com>
2022-06-28 21:23:37 -0400

    Fetching data from github and returning that data

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,6 +1,10 @@
 import { Router, Request, Response } from 'express';
+import { Repo } from '../models/Repo';
+const fetch = require('node-fetch');
 
 export const repos = Router();
+
+const REPOS_GITHUB_URL = 'https://api.github.com/users/silverorange/repos';
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
@@ -8,5 +12,22 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+
+  let reposFromGitHub: Repo[] = [];
+  let reposFromJsonFile: Repo[] = [];
+  await getReposFromGitHub(REPOS_GITHUB_URL).then((data) => {
+    reposFromGitHub = data;
+  });
+
+  res.json(reposFromGitHub);
 });
+
+const getReposFromGitHub = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    const repos = await response.json();
+    return repos;
+  } catch (error) {
+    return [];
+  }
+};
