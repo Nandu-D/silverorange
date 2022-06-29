d682370a0fde9358e149f6bd36c8715468f1cb9b
Nandu Dharmapalan <nandud999@gmail.com>
2022-06-28 23:50:06 -0400

    Moved App component into its own folder. Fetched repos from Express api endpoint.

---
+++ web/src/components/App/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+import './App.css';
+import { Repo } from '../../models/Repo';
+
+export function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [repos, setRepos] = useState<Repo[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/repos')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setRepos(result);
+        },
+        (err) => {
+          setIsLoaded(true);
+          setError(err);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Couldn't load repositories. Please try again.</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return <div>{repos.length}</div>;
+  }
+}
