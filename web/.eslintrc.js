cbc1ca4a4778a89810f1e2e27db6f53977c57c0a
Nandu Dharmapalan <nandud999@gmail.com>
2022-06-29 11:30:27 -0400

    Added padding to webpage and added snake_case to eslint property names to disable errors for property names coming from api

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -82,7 +82,7 @@ const newConfig = Object.assign({}, config, {
             },
             {
               selector: ['property', 'variable'],
-              format: ['camelCase', 'UPPER_CASE'],
+              format: ['camelCase', 'UPPER_CASE', 'snake_case'], //Added snake_case to disable lint error for property names coming from api
             },
             {
               selector: ['function', 'parameter'],
